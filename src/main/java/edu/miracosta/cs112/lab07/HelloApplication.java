d6de8433c58bc633fbd9f02000ba66ed6190c545
schoolCorey <moontribedude@gmail.com>
2024-11-06 04:47:24 +0900

    Completed to be in line with To-Dos in README

--- src/main/java/edu/miracosta/cs112/lab07/HelloApplication.java
+++ src/main/java/edu/miracosta/cs112/lab07/HelloApplication.java
@@ -1,6 +1,7 @@
 package edu.miracosta.cs112.lab07;//package name here depending on your IDE
 
 import javafx.application.Application;  //abstract class used for JavaFX GUI's
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;              //class for GUI window
 import javafx.scene.Scene;              //class for specific view in GUI window
 import javafx.scene.layout.VBox;        //class for layout pane, organized top-to-bottom
@@ -12,6 +13,8 @@ import javafx.event.ActionEvent;        //class for type of event for action (li
 public class HelloApplication extends Application implements EventHandler<ActionEvent>  { //inheriting core functionality + this class will handle events
     /*** GUI COMPONENTS ***/
     private Button button;
+    private int buttonPresses = 0;
+    private String userName = "";
 
     /*** DRIVER main ***/
     public static void main(String[] args) {
@@ -21,20 +24,44 @@ public class HelloApplication extends Application implements EventHandler<Action
     /*** OVERRIDDEN Application METHODS ***/
     @Override
     public void start(Stage primaryStage) throws Exception{ //Application automatically calls this method to run (our) main javafx code. passes in primary stage (main window)
+
+
         //SETUP COMPONENTS
         Label message = new Label("Hi, User!");
+
+        TextField tf = new TextField();
+        tf.setPromptText("Enter name here");
+        tf.setFocusTraversable(false);
+
+        tf.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                setUserName(tf.getText());
+            }
+        });
         button = new Button("Click me"); //or can set text using setText method separately
-        button.setOnAction(this); //who the event handler is (which object/class should handle the event)
+        button.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+
+                incrementButtonPresses();
+                setUserName(tf.getText());
+                message.setText(String.format("Hi #%d, %s!",buttonPresses,userName));
+
+                System.out.println("Hello CS112");
+            }
+        }); //who the event handler is (which object/class should handle the event)
 
         //ADD COMPONENTS
         VBox layout = new VBox(); //simple layout, components are stacked on top of each other in added order
         layout.getChildren().add(message);
+        layout.getChildren().add(tf);
         layout.getChildren().add(button);
 
         //SETUP SCENE AND SHOW
         Scene scene = new Scene(layout, 300, 250); //layout, dimensions of window
         primaryStage.setScene(scene);
-        primaryStage.setTitle("Hello World"); //setting title of main window
+        primaryStage.setTitle("Corey Jimenez"); //setting title of main window
         primaryStage.show();
     }
 
@@ -43,7 +70,16 @@ public class HelloApplication extends Application implements EventHandler<Action
     public void handle(ActionEvent actionEvent) { //generic method used to handle when events occur (like handle button click)
         //good practice to identify source, in case you have multiple event types/sources
         if(actionEvent.getSource() == button) {
-            System.out.println("Hello, JavaFX!");
+            //empty now because we have it in start() so it can effect label...?
         }
+
+    }
+
+    public void incrementButtonPresses(){
+        buttonPresses++;
+    }
+
+    public void setUserName(String newName){
+        userName = newName;
     }
 }
